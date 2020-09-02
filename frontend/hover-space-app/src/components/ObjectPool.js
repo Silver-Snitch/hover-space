e09600948cf67c961c574fb495caf03efbd538f0
Ashish Lal <17bce048@nirmauni.ac.in>
2020-09-02 23:37:02 +0530

    Shifting frontend to another repo

--- frontend/hover-space-app/src/components/ObjectPool.js
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
-import appConfig from '../../src/config/app-config.json';
-const poolData = {
-	UserPoolId: appConfig.userPool,
-	ClientId: appConfig.clientId
-}
-export default new CognitoUserPool(poolData);
-  
